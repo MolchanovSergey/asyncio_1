585c0366ac6499c57236dfc0dd650bc93f4d25b0
MolchanovSergey <molchanov.sergey@htmail.com>
2023-02-04 08:57:06 +0300

    done

---
+++ db.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text
+
+
+PG_DSN = f'postgresql+asyncpg://postgres:postgres@127.0.0.1:5431/asyncio_1'
+engine = create_async_engine(PG_DSN)
+Base = declarative_base(bind=engine)
+# noinspection PyTypeChecker
+Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class PersonModel(Base):
+    __tablename__ = 'people'
+
+    id = Column(Integer, primary_key=True)
+    birth_year = Column(String)
+    eye_color = Column(String)
+    films = Column(Text)
+    gender = Column(String)
+    hair_color = Column(String)
+    height = Column(String)
+    homeworld = Column(String)
+    mass = Column(String)
+    name = Column(String)
+    skin_color = Column(String)
+    species = Column(Text)
+    starships = Column(Text)
+    vehicles = Column(Text)
